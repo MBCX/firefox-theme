4de673cb3493da46a82310d0e7c108fbb780c083
Marcos <63003002+MBCX@users.noreply.github.com>
2021-10-13 14:18:38 -0400

    Started some work for the Light Theme + a demo C# file for testing

---
+++ vs/vs2019/Firefox Theme/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firefox_Theme
+{
+    class Class1
+    {
+        public int TEST = 0;
+
+        private enum PLAYER_STATE
+        {
+            FREE, ATTACK, RUN, HIT, DEATH, STUN        
+        }
+
+        int addNumbers(int number, int number_2)
+        {
+            return number + number_2;
+        }
+
+        void playerStates()
+        {
+            PLAYER_STATE playerState = PLAYER_STATE.ATTACK;
+
+            switch (playerState)
+            {
+                case PLAYER_STATE.FREE: Console.WriteLine("Free"); break;
+                case PLAYER_STATE.RUN: Console.WriteLine("Run"); break;
+                case PLAYER_STATE.ATTACK: Console.WriteLine("Attack"); break;
+                case PLAYER_STATE.HIT: Console.WriteLine("Hit"); break;
+                case PLAYER_STATE.STUN: Console.WriteLine("Stun"); break;
+                case PLAYER_STATE.DEATH: Console.WriteLine("Death"); break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
